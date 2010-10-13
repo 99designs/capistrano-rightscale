a01d6b959ae6984ffe1aa42b22e6e14ddfc049f3
Lachlan Donald <lachlan@ljd.cc>
2010-10-13 15:50:41 +1100

    Changed to two spaces, rather than tabs

--- Rakefile
+++ Rakefile
@@ -1,18 +1,18 @@
 begin
-	require 'jeweler'
-	Jeweler::Tasks.new do |gemspec|
-		gemspec.name = "capistrano-rightscale"
-		gemspec.summary = "An extension for capistrano for integrating roles with RightScale server tags"
-		gemspec.description = "An extension for capistrano for integrating roles with RightScale server tags"
-		gemspec.email = "lachlan@ljd.cc"
-		gemspec.homepage = "http://github.com/99designs/capistrano-rightscale"
-		gemspec.authors = ["Lachlan Donald"]
-		gemspec.add_dependency('capistrano', '>= 2.1.0')
-		gemspec.add_dependency('rightscale-api')
-	end
-	Jeweler::GemcutterTasks.new
+  require 'jeweler'
+  Jeweler::Tasks.new do |gemspec|
+    gemspec.name = "capistrano-rightscale"
+    gemspec.summary = "An extension for capistrano for integrating roles with RightScale server tags"
+    gemspec.description = "An extension for capistrano for integrating roles with RightScale server tags"
+    gemspec.email = "lachlan@ljd.cc"
+    gemspec.homepage = "http://github.com/99designs/capistrano-rightscale"
+    gemspec.authors = ["Lachlan Donald"]
+    gemspec.add_dependency('capistrano', '>= 2.1.0')
+    gemspec.add_dependency('rightscale-api')
+  end
+  Jeweler::GemcutterTasks.new
 rescue LoadError
-	puts "Jeweler, or one of its dependencies, is not available. Install it with: gem install jeweler"
+  puts "Jeweler, or one of its dependencies, is not available. Install it with: gem install jeweler"
 end
 
 
--- lib/capistrano/rightscale.rb
+++ lib/capistrano/rightscale.rb
@@ -1,40 +1,40 @@
 require 'rightscale-api'
 
 module Capistrano
-	class Configuration
-		module Tags
+  class Configuration
+    module Tags
 
-			# Associate a tag in a specific deployment with a role
-			# Examples:
-			#   tag "x99:role=app", :app, :deployment => 45678
-			def tag(which, *args)
-				@rightscale ||= RightScale::Client.new(fetch(:rightscale_account), fetch(:rightscale_username), fetch(:rightscale_password))
-				@deployments ||= @rightscale.deployments
-				@servers ||= @rightscale.servers
+      # Associate a tag in a specific deployment with a role
+      # Examples:
+      #   tag "x99:role=app", :app, :deployment => 45678
+      def tag(which, *args)
+        @rightscale ||= RightScale::Client.new(fetch(:rightscale_account), fetch(:rightscale_username), fetch(:rightscale_password))
+        @deployments ||= @rightscale.deployments
+        @servers ||= @rightscale.servers
 
-				logger.info "querying rightscale for deployment %s" % args[1][:deployment]
-				deployment = @deployments.index.find { |d| d['href'] == rightscale_url("/deployments/%d" % args[1][:deployment]) }
+        logger.info "querying rightscale for deployment %s" % args[1][:deployment]
+        deployment = @deployments.index.find { |d| d['href'] == rightscale_url("/deployments/%d" % args[1][:deployment]) }
 
-				deployment['servers'].each do |server|
-					if server['state'] == 'operational' && tags_for_server(server).include?(which)
-						settings = @servers.settings(server['href'])
-						logger.info "found server %s (%s) with tag %s" % [ settings['dns_name'], server['nickname'], which ]
-						server(settings['dns_name'], *args)
-					end
-				end
-			end
+        deployment['servers'].each do |server|
+          if server['state'] == 'operational' && tags_for_server(server).include?(which)
+            settings = @servers.settings(server['href'])
+            logger.info "found server %s (%s) with tag %s" % [ settings['dns_name'], server['nickname'], which ]
+            server(settings['dns_name'], *args)
+          end
+        end
+      end
 
-			def tags_for_server(server)
-				instance_id = server["current_instance_href"].split('/').last
-				tags = @rightscale.get(rightscale_url("/tags/search.js?resource_href=/ec2_instances/%d" % instance_id))
-				tags.collect { |x| x["name"] }
-			end
+      def tags_for_server(server)
+        instance_id = server["current_instance_href"].split('/').last
+        tags = @rightscale.get(rightscale_url("/tags/search.js?resource_href=/ec2_instances/%d" % instance_id))
+        tags.collect { |x| x["name"] }
+      end
 
-			def rightscale_url(path)
-				"https://my.rightscale.com/api/acct/%d%s" % [ fetch(:rightscale_account), path ]
-			end
-		end
+      def rightscale_url(path)
+        "https://my.rightscale.com/api/acct/%d%s" % [ fetch(:rightscale_account), path ]
+      end
+    end
 
-		include Tags
-	end
+    include Tags
+  end
 end
